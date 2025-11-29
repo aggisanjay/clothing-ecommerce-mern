38f20963c7102aae1b9e92a75da436f4fbb08e48
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-11-29 17:25:27 +0530

    Update API base URL to Vercel deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://clothing-ecommerce-mern.onrender.com/api",
+  baseURL: "https://clothing-ecommerce-mern-hr3u.vercel.app/api",
   withCredentials: true
 });
 
