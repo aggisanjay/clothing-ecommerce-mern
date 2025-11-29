d8901bb7f508f995df78d791488237b8a7558b41
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-11-29 15:58:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://clothing-ecommerce-mern.vercel.app",
   credentials:true
 }));
 
