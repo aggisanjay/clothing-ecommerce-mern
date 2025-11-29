b5e27c6690bc21ff7268d6841120cb90770a7a72
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-11-29 19:46:01 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, me } from "../controllers/authController.js";
+import { register, login, me,logout } from "../controllers/authController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
